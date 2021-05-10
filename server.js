0bb20c9b21601cad6609923185c25d1530c6dabc
AllenChenRR <allenchenrr@gmail.com>
2021-05-10 14:46:35 -0700

    Removes the foot notes and newlines characters

--- server.js
+++ server.js
@@ -3,12 +3,42 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const { data } = require('cheerio/lib/api/attributes');
+const { remove } = require('cheerio/lib/api/manipulation');
 app = express();
 
 app.enable('trust proxy');
 app.use(express.json());
 
 // app.use('/', require('./index'));
+
+function removeFootnotes (text) {
+  var skip = false;
+  var newText = [];
+  for (var i = 0; i < text.length; i++) {
+    if (skip && text.charAt(i) == ']') {
+      skip = false; 
+      continue;
+    }
+    if (text.charAt(i) == '[') {
+      skip = true;
+      continue;
+    }
+    if (skip) {
+      continue;
+    }
+    newText.push(text.charAt(i));
+  }
+  return newText.join('');
+}
+
+function removeNewline(text) {
+  return text.replace(/\n/g, "");
+}
+
+function removeExtraChars(text) {
+  var noFootnotes = removeFootnotes(text);
+  return removeNewline(noFootnotes)
+}
 
 function getWikiData (html) {
   const data = [];
@@ -17,14 +47,15 @@ function getWikiData (html) {
   var tempIntro = $(".mw-parser-output p").first();
   console.log(typeof tempIntro.text())
   var introContent = ""
-  while($(tempIntro).prop("tagName") == "P") {
+  while ($(tempIntro).prop("tagName") == "P") {
     introContent += $(tempIntro).text()
     tempIntro = $(tempIntro).next();
   }
+
   data.push(
     {
       header: "Intro",
-      content:  introContent
+      content:  removeExtraChars(introContent)
     }
   )
 
@@ -33,13 +64,25 @@ function getWikiData (html) {
     group.header = $(elem).text();
     group.content = "";
     var tempElem = $(elem).next();
+    console.log(`${group.header} ${$(tempElem)} group ${JSON.stringify(group)}`)
     // Loops through all <p> elements following the header until another header is reached
-    while ($(tempElem).prop("tagName") == "P") {
-      group.content += $(tempElem).text();
-      tempElem = $(tempElem).next()
-    }
+    // if ($(tempElem).length !== 0) {
+    //   while ($(tempElem).prop("tagName") != "H2") {
+    //     if ($(tempElem).prop("tagName") == "P") {
+    //       group.content += $(tempElem).text();
+    //     }
+    //     tempElem = $(tempElem).next()
+    //   }
 
-    data.push(group)
+    if ($(tempElem).length !== 0) {
+      while ($(tempElem).prop("tagName") == "P") {
+        group.content += $(tempElem).text();
+        tempElem = $(tempElem).next()
+      }
+      group.header = removeExtraChars(group.header);
+      group.content = removeExtraChars(group.content);
+      data.push(group)
+    }
   })
   var scrapedResult = {
     data: data
@@ -49,18 +92,23 @@ function getWikiData (html) {
 }
 
 app.get('/', (req, res) => {
-  const url = req.body.url;
+  res.status(200).send("CS361 Text Scraper by Allen Chen")
+})
+
+app.get('/data', (req, res) => {
+  const url = req.query.url;
+  console.log(typeof url)
   axios.get(url)
     .then(response => {
-      fs.writeFile('responseData.html', response.data, (err) => {
-        if (err) throw err;
-      });
+      // fs.writeFile('responseData.html', response.data, (err) => {
+      //   if (err) throw err;
+      // });
       // let getData = html => {
       //   data = [];
       //   const $ = cheerio.load(html);
       //   console.log($)
       // }
-      return res.status(200).send(getWikiData(response.data));
+      return res.status(200).json(getWikiData(response.data));
       // const $ = cheerio.load(response.data);
       // console.log($.html())
     })
